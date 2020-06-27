c8f36c99578b51dfcbfed1401ee3489a6661497c
cmogush <cmogush@gmail.com>
2020-06-27 16:17:34 -0400

    Ready build - Add move_files fn
    The build has been tested and is ready for production.
    The move_files function is added to move all modified files
    to a new directory for easy backup and migration

--- find_replace_in_files.py
+++ find_replace_in_files.py
@@ -1,33 +1,23 @@
-import os
-import re
+import os, re, shutil
 from pathlib import Path
 
-# def iterate(dir):
-#     """iterate over the directory"""
-#     for entry in os.listdir(dir):
-#         currObj = os.path.join(dir, entry)
-#
-#         # get the filetype
-#         if not os.path.isdir()
-#         f, e = os.path.splitext(entry)
-#
-#         # directories, iterate
-#         if os.path.isdir(currObj):
-#             iterate(currObj, swfOnly, evenOdd)
-#
-#         # html, convert
-#         elif e[1] == ".html":
-#             convertHTML(currObj)
-#             # print(entry+" : html")
-#
-#         # config, convert
-#         elif entry == "config.xml":
-#             convertConfig(currObj, swfOnly, evenOdd)
-#             # print(entry+" : config")
-#
-#         else:
-#             continue
-#         evenOdd += 1
+filetype = ".html"
+replacements = {
+    r'http://vjs.zencdn.net/ie8/1.1.0/videojs-ie8.min.js': r'https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js',
+    r'http://vjs.zencdn.net/5.0.2/video.js': r'https://vjs.zencdn.net/7.8.2/video.js'}
+
+def replace_in_files(parent, dir):
+    """makes replacements in all files in a directory, returns a list of all files updated"""
+    log = []
+    full_path = os.path.join(parent, dir)
+    for entry in os.listdir(full_path):
+        entry_full_path = os.path.join(full_path, entry)
+        if os.path.isdir(entry_full_path):
+            replace_in_files(full_path, entry)
+        elif filetype_matches(entry, filetype):
+            if make_replacements(entry_full_path, replacements):
+                log.append(entry_full_path)
+    return log
 
 def filetype_matches(file, filetype):
     """returns True if filetype matches"""
@@ -39,6 +29,8 @@ def filetype_matches(file, filetype):
 
 def make_replacements(file, replacements):
     """takes in a file and a dictionary of patterns : replacements to enact"""
+    """returns True if replacements made, False if no change"""
+    match = False
     # get content
     with open(file, 'r') as f:
         content = f.read()
@@ -56,17 +48,44 @@ def make_replacements(file, replacements):
             return True
     return False
 
+def do_replace(pattern, replacement, string):
+    """helper function for move_files"""
+    if string.startswith(pattern):
+        return replacement + string[len(pattern):]
+
+    return string
+
+def move_files(list, pattern, replacement):
+    """takes a list of files and moves them to another target location"""
+    dest_list = [] # list of destinations
+    # setup the new destination list
+    for entry in list:
+        dest = do_replace(pattern,replacement,entry)
+        dest_list.append(dest)
+    # make the moves
+    for file, dest in zip(list, dest_list):
+        new_path = shutil.copyfile(file.strip(' \n'), dest)
+        print(new_path)
+
 def main():
-    pattern = r""
-    replacement = r""
-    print("Enter local directory to update ")
-    targetDir = Path(input("Input: "))
-    # targetDir = Path(r"D:\Edison\n2k_ALL\SC\Biology N2K")
-    parentDir = str(targetDir.parent)
-    dir = str(os.path.basename(targetDir))
-
-
-    iterate(targetDir)
+    # print("Enter local directory to update ")
+    # dir = Path(input("Input: "))
+    dir = r'C:\Users\Chris\Desktop\Python Scripts\find_replace_in_files\big_test'
+    log = replace_in_files("", dir)
+    print(log)
+    outfile = "change_log.txt"
+    with open(os.path.join(dir, outfile), 'w') as f:
+        f.writelines('%s\n' % item for item in log)
+    print("writing result to {}\{}".format(dir, outfile))
+    new_dest = r'C:\Users\Chris\Desktop\Python Scripts\find_replace_in_files\dest\big final_dest'
+    move_files(log, dir, new_dest)
+    outfile_moved = "change_log_moved.txt"
+    with open(os.path.join(dir, outfile_moved), 'w') as f:
+        f.writelines('%s\n' % item for item in log)
+    print("writing result to {}\{}".format(dir, outfile_moved))
+    with open(os.path.join(new_dest, outfile_moved), 'w') as f:
+        f.writelines('%s\n' % item for item in log)
+    print("writing result to {}\{}".format(new_dest, outfile_moved))
 
 if __name__ == "__main__":
     main()
--- find_replace_in_files_test.py
+++ find_replace_in_files_test.py
@@ -1,25 +1,41 @@
-import unittest
-from find_replace_in_files import filetype_matches, make_replacements
+import unittest, os
+from find_replace_in_files import filetype_matches, make_replacements, move_files
 
-# test find file type
-# should be able to specify a filetype and only make find/replacements in that type
+
 class MyTestCase(unittest.TestCase):
+    # test find file type
+    # should be able to specify a filetype and only make find/replacements in that type
     def test_filetype_matches(self):
-        file = r"C:\Users\Chris\Desktop\Python Scripts\find_replace_in_files\test files\TV.Cooney.01.html"
+        file = r"C:\Users\Chris\Desktop\Python Scripts\find_replace_in_files\test files\TV.Cieply.01.html"
         self.assertEqual(filetype_matches(file, ".html"), True)
 
     def test_filetype_matches(self):
         file = r"C:\Users\Chris\Desktop\Python Scripts\find_replace_in_files\test files\New Picture.BMP"
         self.assertEqual(filetype_matches(file, ".html"), False)
 
-# test replacement in file type
-# should be able to make the replacement and output a report of the file, location, where the replacement was made
+    # test replacement in file type
+    # should be able to make the replacement and output a report of the file, location, where the replacement was made
     def test_make_replacements(self):
-        file = r"C:\Users\Chris\Desktop\Python Scripts\find_replace_in_files\test files\TV.Cooney.01.html"
+        file = r"C:\Users\Chris\Desktop\Python Scripts\find_replace_in_files\test files\TV.Cieply.01.html"
+        with open(file, 'r') as f:
+            content = f.read()
         replacements = {'http://vjs.zencdn.net/ie8/1.1.0/videojs-ie8.min.js':
                             'https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js',
                         'http://vjs.zencdn.net/5.0.2/video.js': 'https://vjs.zencdn.net/7.8.2/video.js'}
         self.assertEqual(make_replacements(file, replacements), True)
+        # reset the file
+        with open(file, 'w') as f:
+            f.write(content)
+
+    #test moving files based on the log
+    def test_move_files(self):
+        log = [r'C:\Users\Chris\Desktop\Python Scripts\find_replace_in_files\test files\TV.Cieply.01.html']
+        pattern = r'C:\Users\Chris\Desktop\Python Scripts\find_replace_in_files\test files'
+        replacement = r'C:\Users\Chris\Desktop\Python Scripts\find_replace_in_files\dest\final_dest'
+        move_files(log, pattern, replacement)
+        destFile = r'C:\Users\Chris\Desktop\Python Scripts\find_replace_in_files\dest\final_dest\TV.Cieply.01.html'
+        self.assertEqual(os.path.isfile(destFile), True)
+        os.remove(destFile)
 
 if __name__ == '__main__':
     unittest.main()
